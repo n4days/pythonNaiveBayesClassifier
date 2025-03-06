6453ee83d4706eb7b8c9c1c69c5f8525fd9983b4
n4days <alirsyhad.ramadhan49@gmail.com>
2025-03-06 10:35:32 +0700

    membuat dataset baru

--- cleaning.py
+++ cleaning.py
@@ -2,7 +2,7 @@ import pandas as pd
 import string
 
 # Load the data from the CSV file
-file_path = 'newdataset.csv'
+file_path = 'newdataset2.csv'
 df = pd.read_csv(file_path)
 
 # Function to clean the comment text
@@ -28,4 +28,4 @@ df['Words'] = df['text'].apply(lambda x: x.split())
 print(df[['Words', 'kategori']].head())
 
 # Export the 'Words' column to an Excel file
-df[['Words', 'kategori']].to_excel('Words.xlsx', index=False)
+df[['Words', 'kategori']].to_excel('Words2.xlsx', index=False)
